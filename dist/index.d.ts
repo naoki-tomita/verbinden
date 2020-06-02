d595103a854fd74b7eb82801606a2fb17870cd76
kojiro.ueda <mxcpy608@hotmail.com>
2020-06-02 16:01:00 +0900

    rm dist

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Verbinden } from "./client";
-export { listen } from "./server";
-//# sourceMappingURL=index.d.ts.map
--- dist/server/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function listen(port?: number): void;
-//# sourceMappingURL=index.d.ts.map
